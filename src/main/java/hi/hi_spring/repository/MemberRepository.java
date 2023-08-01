f7c7d2d4be0c4499a86f8438b98ee870ec24c865
hayun <hs183000@naver.com>
2023-08-01 22:38:26 +0900

    회원 도메인과 리포지토리 만들기

---
+++ src/main/java/hi/hi_spring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package hi.hi_spring.repository;
+
+import hi.hi_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    // null일경우 그래도 받는 것 보다는 Optional을 감싸서 받는 방법사용
+    List<Member> findALl();
+}
---
+++ src/main/java/hi/hi_spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package hi.hi_spring.repository;
+
+import hi.hi_spring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private  static long sequence = 0L;
+    // 키값을 0,1,2 생성
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+        // null이 반횐되어도 클라이언트 쪽에서 해결 가능하게 Optional로 감싸준다.
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+        // member에서 membergetName이 파라메터에 넘어온 name과 같은지 확인하여 같은경우 반환됨
+    }
+
+    @Override
+    public List<Member> findALl() {
+        return new ArrayList<>(store.values());
+        //맵의 값을 리스트에 넣어 반환
+    }
+}
