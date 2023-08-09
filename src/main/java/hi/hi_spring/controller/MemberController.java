42ad2437ecf204a7effa8a4bfc15c7dfe59df18c
hayun <hs183000@naver.com>
2023-08-09 23:23:24 +0900

    회원 등록

--- src/main/java/hi/hi_spring/controller/MemberController.java
+++ src/main/java/hi/hi_spring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hi.hi_spring.controller;
 
+import hi.hi_spring.domain.Member;
 import hi.hi_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController{
@@ -17,5 +20,21 @@ public class MemberController{
     @Autowired // 연결시킬때 Autowired 사용 , 생성자 주입
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+
+        Member member = new Member();
+        member.setName(form.getName());
+
+     //   System.out.println("member = " + member.getName());
+     //   입력시 member = spring
+        memberService.join(member);
+        return "redirect:/";
     }
 }
