2d2194d5f218da6091d03b3a46e252d21fb0b2d3
hayun <hs183000@naver.com>
2023-08-11 23:29:04 +0900

    데이터베이스 설치 및 JDBC

--- src/main/java/hi/hi_spring/service/SpringConfig.java
+++ src/main/java/hi/hi_spring/service/SpringConfig.java
@@ -1,14 +1,23 @@
 package hi.hi_spring.service;
 
+import hi.hi_spring.repository.JdbcMemberRepository;
 import hi.hi_spring.repository.MemberRepository;
 import hi.hi_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
 
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
         @Bean
         public MemberService memberService(){
             return new MemberService(memberRepository());
@@ -16,7 +25,7 @@ public class SpringConfig {
 
         @Bean
         public MemberRepository memberRepository(){
-            return new MemoryMemberRepository();
+            return new JdbcMemberRepository(dataSource);
         }
     }
 
