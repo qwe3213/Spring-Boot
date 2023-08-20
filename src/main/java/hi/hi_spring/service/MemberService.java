122868e6de201458428fe209cbc2e87e2e63f15f
hayun <hs183000@naver.com>
2023-08-20 23:29:26 +0900

    AOP 적용

--- src/main/java/hi/hi_spring/service/MemberService.java
+++ src/main/java/hi/hi_spring/service/MemberService.java
@@ -25,19 +25,10 @@ public class MemberService {
     * 회원 가입
     * */
     public Long join(Member member){
-     // 같은 이름이 있는 중복 회원X
-
-        long start = System.currentTimeMillis();
-
-        try {
+          // 같은 이름이 있는 중복 회원X
             validateDuplicateMember(member);
             memberRepository.save(member);
             return member.getId();
-        } finally {
-           long finish = System.currentTimeMillis();
-           long timeMs = finish - start;
-           System.out.println("join = " + timeMs + "ms");
-        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -53,14 +44,7 @@ public class MemberService {
     * */
 
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-        try {
             return memberRepository.findALl();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs +"ms");
-        }
     }
 
     public Optional<Member> findOne(Long memberId){
--- src/main/java/hi/hi_spring/service/SpringConfig.java
+++ src/main/java/hi/hi_spring/service/SpringConfig.java
@@ -1,5 +1,6 @@
 package hi.hi_spring.service;
 
+import hi.hi_spring.aop.TimeTraceAop;
 import hi.hi_spring.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -21,7 +22,14 @@ public class SpringConfig {
         public MemberService memberService(){
             return new MemberService(memberRepository);
         }
-        // @Bean
+
+      //    @Bean
+      //    public TimeTraceAop timeTraceAop(){
+      //    return new TimeTraceAop();
+      //   }
+      //  timeTraceAop에서 @Component를 하지 않을경우 사용
+
+    //    @Bean
     //    public MemberRepository memberRepository(){
 
     //     return new MemoryMemberRepository();
